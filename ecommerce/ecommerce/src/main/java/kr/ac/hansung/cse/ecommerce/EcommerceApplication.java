c4cdcce70ee0d054d13d5bdcc5a7ae17d100acfc
jeongHyeonu <114675420+jeongHyeonu@users.noreply.github.com>
2024-08-20 18:25:53 +0900

    과제2 : e-commerce API

---
+++ ecommerce/ecommerce/src/main/java/kr/ac/hansung/cse/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+
+}
---
+++ ecommerce/ecommerce/src/test/java/kr/ac/hansung/cse/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
